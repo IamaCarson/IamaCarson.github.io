37e19b5dd926f8bd1129e4730eb28d336c8bff29
aCarson <89594536+IamaCarson@users.noreply.github.com>
2022-01-30 02:32:04 -0600

    Add files via upload

--- splashes.js
+++ splashes.js
@@ -1,5 +1,5 @@
 
-const splashList = ['2!', 'Oh boy!', 'American Cheese', 'Also try yugoslavia.best!', 'I <3 Videogames!', 'Barson', 'amogos', 'Arson'];
+const splashList = ['2!', 'Oh boy!', 'American Cheese', 'Also try yugoslavia.best!', 'I <3 Videogames!', 'Barson', 'amogos', 'Arson', 'beangs', 'fecal funnies', 'wed!', '420!', 'cardbox was here :)', '9 + 10'];
 
 function splash() {
     document.getElementById('splash').innerText = splashList[Math.floor(Math.random()*splashList.length)];
