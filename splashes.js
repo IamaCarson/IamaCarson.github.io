2abb1368b50150f9cbbca1cf3a189f44d867581c
aCarson <89594536+IamaCarson@users.noreply.github.com>
2022-01-30 01:21:07 -0600

    Add files via upload

--- splashes.js
+++ splashes.js
@@ -1,5 +1,5 @@
 
-const splashList = ['2!', 'Oh boy!', 'American Cheese', 'Also try yugoslavia.best!', 'I <3 Videogames!'];
+const splashList = ['2!', 'Oh boy!', 'American Cheese', 'Also try yugoslavia.best!', 'I <3 Videogames!', 'Barson', 'amogos', 'Arson'];
 
 function splash() {
     document.getElementById('splash').innerText = splashList[Math.floor(Math.random()*splashList.length)];
